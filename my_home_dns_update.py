fd6d29cfee21db27bccfd036a91a206a0905c0d4
backroot <kotaro_saito@noah.jp>
2017-11-05 11:44:48 +0900

    beta release

---
+++ my_home_dns_update.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import yaml, boto, urllib2, json
+
+def main():
+    with open('config.yaml', 'rt') as f:
+        data = f.read()
+    
+    config = yaml.safe_load(data)
+    service = config['ip_check_service']
+
+    try:
+        url = urllib2.urlopen(service)
+        res = json.loads(url.read())
+    finally:
+        url.close()
+    
+    route53_conn = boto.connect_route53(config['key'], config['secret'])
+    zone = route53_conn.get_zone(config['domain_name'])
+    record = zone.get_a(config['zone_name'])
+    a = record.to_print() 
+    if a != res['ip']:
+        zone.update_a(config['zone_name'], res['ip'])
+
+if __name__ == '__main__':
+    main()
